7c1f831be3a6307447470279c736488beda3dee9
BraylenC C <braylen.celestine@sciacademy.org>
2023-09-14 15:12:01 +0000

    add index.html and css files

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,16 +43,19 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
-
+        createPlatform(400,300,250,10)
+        createPlatform(250,400,150,10)
+        createPlatform(50,150,250,10)
+        createPlatform(300,200,150,10)
+        createPlatform()
     
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
+         createCollectable("grace",450,700,0)
+         createCollectable("grace",)
 
 
 
@@ -61,7 +64,8 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
+        createCannon("right",750,1500,90)
+        createCannon("top",700,1400,70)
 
 
 
